b3fa072da3bd065a09397687064364bceb130d35
jansode <63640610+jansode@users.noreply.github.com>
2024-09-03 12:47:32 +0300

    Add to startup script for upcoming feature.

--- russian_training.bat
+++ russian_training.bat
@@ -2,3 +2,5 @@
 
 if %1%==custom python vocabulary.py session custom_words.csv
 if %1%==common python vocabulary.py session most_common_words.csv
+if %1%==custom_eng python vocabulary.py session_eng custom_words.csv
+if %1%==common_eng python vocabulary.py session_eng most_common_words.csv
